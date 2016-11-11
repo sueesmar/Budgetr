cf5e4cd703315700f809c7536a4f8744079b18da
Cengiz Cetinkaya <cengiz.cetinkaya@gmail.com>
2016-11-11 23:31:28 +0100

    receipt capture: cameraavtivity and the layout added

---
+++ app/src/main/java/ch/msengineering/budgetr/CameraActivity.java
@@ -0,0 +1,41 @@
+package ch.msengineering.budgetr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by Cengiz on 11.11.16.
+ */
+
+public class CameraActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        this.imageView = (ImageView)this.findViewById(R.id.imgPicture);
+        Button photoButton = (Button) this.findViewById(R.id.bCapture);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}
